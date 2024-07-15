f43d61341913dd6389013feca8103e9f84006ae5
Viktor Shveda <vinix.ru@gmail.com>
2024-07-15 11:51:23 +0300

    Fixed DriverDetailView class and imports

--- taxi/views.py
+++ taxi/views.py
@@ -40,4 +40,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.select_related("manufacturer")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
